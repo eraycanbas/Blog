b76663d4a1706fed5c7ffb4f91a9bca1f80b2d38
Eray Can Baş <eraycanbas@hotmail.com.tr>
2025-03-01 22:55:38 +0300

    .

--- Blog.API/Controllers/BlogPostController.cs
+++ Blog.API/Controllers/BlogPostController.cs
@@ -23,39 +23,5 @@ namespace Blog.API.Controllers
             var id = await _mediator.Send(command);
             return Ok(new { Message = "Blog post created successfully." });
         }
-
-        //[HttpGet]
-        //[Route("api/blogposts/{id}")]
-        //public async Task<IActionResult> GetBlogPostById(int id)
-        //{
-        //    var query = new GetBlogPostByIdQuery { BlogPostId = id };
-        //    var blogPost = await _getBlogPostByIdQueryHandler.HandleAsync(query);
-
-        //    if (blogPost == null)
-        //        return NotFound(new { Message = "Blog post not found." });
-
-        //    return Ok(blogPost);
-        //}
-
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> UpdateBlogPost(int id, [FromBody] UpdateBlogPostCommand command)
-        //{
-        //    if (!ModelState.IsValid)
-        //        return BadRequest(ModelState);
-
-        //    if (id != command.BlogPostId)
-        //        return BadRequest("BlogPostId in the URL and body do not match.");
-
-        //    await _updateBlogPostCommandHandler.HandleAsync(command);
-        //    return Ok(new { Message = "Blog post updated successfully." });
-        //}
-
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteBlogPost(int id)
-        //{
-        //    var command = new DeleteBlogPostCommand { BlogPostId = id };
-        //    await _deleteBlogPostCommandHandler.HandleAsync(command);
-        //    return Ok(new { Message = "Blog post deleted successfully." });
-        //}
     }
 }
